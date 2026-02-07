1fa80f74b6f0a58df02f218e0925b0d3a303124b
Rob Coekaerts <roco@odoo.com>
2026-02-07 19:56:30 +0100

    Clarified input tests.

--- 05-strings/06-assignment-interactive-greet/test.py
+++ 05-strings/06-assignment-interactive-greet/test.py
@@ -1,7 +1,12 @@
 import time
 time.sleep(10)
 import pytest
-import student
+
+from unittest.mock import patch
+def raise_error_on_input(prompt = None):
+    raise ValueError("The student.py file should only contain the function definition. It cannot contain a function call to the function you implemented.")
+with patch("builtins.input", new=raise_error_on_input):
+    import student
 
 
 @pytest.fixture
@@ -32,17 +37,17 @@ def test_interactive_greet(fake_input, capsys, name, expected):
     assert expected == actual
 
 
-@pytest.mark.timeout(1)
-def test_interactive_greet_calls_greet(monkeypatch, fake_input, capsys):
-    def fake_greet(name):
-        return f"Bye, {name}"
+# @pytest.mark.timeout(1)
+# def test_interactive_greet_calls_greet(monkeypatch, fake_input, capsys):
+#     def fake_greet(name):
+#         return f"Bye, {name}"
 
-    fake_input(["Hannibal"])
-    monkeypatch.setattr('student.greet', fake_greet)
+#     fake_input(["Hannibal"])
+#     monkeypatch.setattr('student.greet', fake_greet)
 
-    student.interactive_greet()
+#     student.interactive_greet()
 
-    captured = capsys.readouterr()
-    actual = captured.out
-    expected = "Bye, Hannibal\n"
-    assert expected == actual, "interactive_greet should rely on greet"
+#     captured = capsys.readouterr()
+#     actual = captured.out
+#     expected = "Bye, Hannibal\n"
+#     assert expected == actual, "interactive_greet should rely on greet"
--- 06-loops/04-assignment-sum-input/test.py
+++ 06-loops/04-assignment-sum-input/test.py
@@ -1,7 +1,12 @@
 import time
 time.sleep(10)
 import pytest
-import student
+
+from unittest.mock import patch
+def raise_error_on_input(prompt = None):
+    raise ValueError("The student.py file should only contain the function definition. It cannot contain a function call to the function you implemented.")
+with patch("builtins.input", new=raise_error_on_input):
+    import student
 
 
 @pytest.fixture
